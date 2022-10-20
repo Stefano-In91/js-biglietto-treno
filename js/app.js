164c054ed63db3d847f00a0070b41b70f8d62ede
Stefano.In91 <ste.ince91@gmail.com>
2022-10-20 13:39:44 +0200

    fixes spacing in js code - Final

--- js/app.js
+++ js/app.js
@@ -4,22 +4,24 @@ const runKm = Number(prompt( "Quanti km devi percorrere?" ));
 const passengerAge = Number(prompt( "La tua età" ));
 
 console.log( `${passengerAge} anni ${runKm} km` )
+
 // Prezzo del biglietto definito in base ai km (0.21 € al km).
 const priceXkm = 0.21;
 let runPrice = runKm * priceXkm;
 
 console.log(`${runPrice} prezzo corsa iniziale`);
 
+// Calcolo sconti per fasce d'età
 if ( passengerAge < 18 ) {
    // Applicato uno sconto del 20% per i minorenni.
    runPrice = runPrice * 0.8;
    console.log( `${runPrice} prezzo corsa dopo lo sconto` );
-
 } else if ( passengerAge >= 65 ) {
    // Applicato uno sconto del 40% per gli over 65.
    runPrice = runPrice * 0.6;
    console.log( `${runPrice} prezzo corsa dopo lo sconto` );
 }
+
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 runPrice = parseFloat(runPrice).toFixed(2);
 console.log( `${runPrice} prezzo corsa finale` );
